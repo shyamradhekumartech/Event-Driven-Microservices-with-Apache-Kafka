8ab47560a6bc3b688f56743c9e95af01054fa949
sandeeprawat983 <rawat683sandeep@gmail.com>
2026-05-27 17:44:29 +0530

    send message asynchronously by kafka template

--- section6/ProductsMicroservice/src/main/java/com/products/ws/service/ProductServiceImpl.java
+++ section6/ProductsMicroservice/src/main/java/com/products/ws/service/ProductServiceImpl.java
@@ -1,14 +1,53 @@
 package com.products.ws.service;
 
 import com.products.ws.rest.CreateProductRestModel;
+import com.products.ws.rest.ProductCreatedEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
 
 @Service
 public class ProductServiceImpl implements ProductService {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(ProductServiceImpl.class);
+
+    private final KafkaTemplate<String, ProductCreatedEvent> kafkaTemplate;
+
+    public ProductServiceImpl(KafkaTemplate<String, ProductCreatedEvent> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
     @Override
     public String createProduct(CreateProductRestModel productRestModel) {
-        return "";
+        String productId = UUID.randomUUID().toString();
+
+        // TODO: Persist Product Details into database table before publishing an event
+
+        ProductCreatedEvent productCreatedEvent = new ProductCreatedEvent(
+                productId,
+                productRestModel.getTitle(),
+                productRestModel.getPrice(),
+                productRestModel.getQuantity()
+        );
+
+        CompletableFuture<SendResult<String, ProductCreatedEvent>> future = kafkaTemplate.send("product-created-events-topic", productId, productCreatedEvent);
+
+        future.whenComplete((result, exception) -> {
+            if (exception != null) {
+                LOGGER.error("Failed to send message: {}", exception.getMessage());
+            } else {
+                LOGGER.info("Message send successfully: {}", result.getRecordMetadata());
+            }
+        });
+
+        LOGGER.info("Returning product id");
+
+        return productId;
     }
 
 }
